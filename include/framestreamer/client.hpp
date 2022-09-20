321a669379b79e375bcfee1341bd4a21954298bb
Anna Panfil <apanfil@internships.antmicro.com>
2022-09-20 13:08:17 +0200

    [#37194] client: Move class definition to header
    Signed-off-by: Anna Panfil <apanfil@internships.antmicro.com>

---
+++ include/framestreamer/client.hpp
@@ -0,0 +1,67 @@
+#pragma once
+#include "framestreamer/framereceiver.hpp"
+#include <GL/glew.h> //GLuint
+
+/**
+ * Configuration of current program execution
+ */
+typedef struct Config
+{
+    std::string ip = ""; ///< my ip address
+    int port = 1100;     ///< my port
+} Config;
+
+
+/**
+ * Class for handling displaying frames and loading their textures
+ */
+class FrameWindow
+{
+public:
+    /**
+     * Default, empty constructor
+     */
+    FrameWindow() {}
+
+    /**
+     * Constructor. Sets name and image
+     *
+     * @param frame Frame structure – source of the name and image
+     */
+    FrameWindow(Frame &frame) : name(frame.name), img(frame.img), changed(true) {}
+
+    /**
+     * Replaces img and marks it as changed
+     *
+     * The method can be launched from different thread, so the reloadTexture function is not used. You have to run it
+     * manually from the main thread.
+     *
+     * @param new_image New image
+     */
+    void changeImg(cv::Mat &new_image);
+
+    /**
+     * Reloads the texture from the img. Creates a texture handler if it's not present.
+     * If the image has 1 channel it's assumed to be grayscale (and it's converted to BGR), otherwise it's treated like
+     * BGR.
+     *
+     * Has to be run from the same thread as `glfwInit` (the main one)
+     */
+    void reloadTexture();
+
+    /**
+     * Prepares Dear ImGui window with the image
+     *
+     * @param frame Frame to display
+     */
+    void display();
+
+    ~FrameWindow();
+
+private:
+    GLuint texture = -1;  ///< OpenGL texture identifier
+    std::string name;     ///< Window name
+    cv::Mat img;          ///< Image to display
+    bool changed = false; ///< If the img has changed since last texture reload
+};
+
--- src/client.cpp
+++ src/client.cpp
@@ -1,11 +1,11 @@
-#include "framestreamer/framereceiver.hpp"
 #include "framestreamer/streamexception.hpp"
+
+#include "framestreamer/client.hpp"
 
 #include "cxxopts/cxxopts.hpp"
 #include "framestreamer/utils.hpp"
 #include "imgui/backends/imgui_impl_glfw.h"
 #include "imgui/backends/imgui_impl_opengl3.h"
-#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <opencv2/imgproc.hpp>
 #include <signal.h>
@@ -17,58 +17,6 @@
  * Client receives and shows streams
  */
 
-/**
- * Class for handling displaying frames and loading their textures
- */
-class FrameWindow
-{
-public:
-    /**
-     * Default, empty constructor
-     */
-    FrameWindow() {}
-
-    /**
-     * Constructor. Sets name and image
-     *
-     * @param frame Frame structure – source of the name and image
-     */
-    FrameWindow(Frame &frame) : name(frame.name), img(frame.img), changed(true) {}
-
-    /**
-     * Replaces img and marks it as changed
-     *
-     * The method can be launched from different thread, so the reloadTexture function is not used. You have to run it
-     * manually from the main thread.
-     *
-     * @param new_image New image
-     */
-    void changeImg(cv::Mat &new_image);
-
-    /**
-     * Reloads the texture from the img. Creates a texture handler if it's not present.
-     * If the image has 1 channel it's assumed to be grayscale (and it's converted to BGR), otherwise it's treated like
-     * BGR.
-     *
-     * Has to be run from the same thread as `glfwInit` (the main one)
-     */
-    void reloadTexture();
-
-    /**
-     * Prepares Dear ImGui window with the image
-     *
-     * @param frame Frame to display
-     */
-    void display();
-
-    ~FrameWindow();
-
-private:
-    GLuint texture = -1;  ///< OpenGL texture identifier
-    std::string name;     ///< Window name
-    cv::Mat img;          ///< Image to display
-    bool changed = false; ///< If the img has changed since last texture reload
-};
 
 void FrameWindow::reloadTexture()
 {
@@ -122,15 +70,6 @@ void FrameWindow::display()
 FrameWindow::~FrameWindow() { glDeleteTextures(1, &texture); }
 
 //------------------ COMMAND LINE OPTIONS ----------------------
-/**
- * Configuration of current program execution
- */
-typedef struct Config
-{
-    std::string ip = ""; ///< my ip address
-    int port = 1100;     ///< my port
-} Config;
-
 /**
  * Parses command line options
  *
