ad1671141f949d53d103ed5a5fc1e6240586f5c1
Krzysztof Wasielewski <kwasielewski@internships.antmicro.com>
2023-03-31 08:42:36 +0200

    [#42501] python-bindings: Removed cvnp utility functions
    Signed-off-by: Krzysztof Wasielewski <kwasielewski@internships.antmicro.com>

--- src/python-bindings/main.cpp
+++ src/python-bindings/main.cpp
@@ -1,5 +1,5 @@
 #include <pybind11/pybind11.h>
-#include "cvnp/cvnp.h"
+#include <cvnp/cvnp.h>
 
 namespace py = pybind11;
 
@@ -7,12 +7,10 @@ void initStreamException(py::module &);
 void initUdpInterface(py::module &);
 void initFrameSender(py::module &);
 void initFrameReceiver(py::module &);
-void pydef_cvnp(py::module &);
 
 PYBIND11_MODULE(farshow, m){
     initStreamException(m);
     initUdpInterface(m);
     initFrameSender(m);
     initFrameReceiver(m);
-    pydef_cvnp(m);
 }
